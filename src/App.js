590bcb2555e44b25a832e60424ddd72daba56e05
kunalArya1 <kunalkrraj220@gmail.com>
2022-11-14 06:24:33 +0530

    first_react_app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './style.css'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <div>
+            <div className='form'>
+                <form action="">
+                    <p>FirstName</p>
+                    <input className='input' type="text" placeholder='FirstName' /><br />
+                    <p>LastName</p>
+                    <input className='input' type="text" placeholder='LastName' />
+                    <br />
+                    <p>Password</p>
+                    <input className='input' type="password" placeholder='Password' />
+                    <br />
+                    <br />
+                    <input type="submit" />
+                    <br />
+                </form> 
+            </div>
+        {/* <h1 style={{ textAlign: "center" }}>Kunal Arya</h1> */}
+        </div>
+    );
 }
 
 export default App;
